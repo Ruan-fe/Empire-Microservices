44f47881380c1857767ab052cfef31a656bd078f
Ruan-fe <55806119+Ruan-fe@users.noreply.github.com>
2021-02-22 21:51:22 -0300

    De @Configuration para @EnableWebSecurity

--- authentication/src/main/java/com/empire/authentication/configurations/security/SecurityConfig.java
+++ authentication/src/main/java/com/empire/authentication/configurations/security/SecurityConfig.java
@@ -4,16 +4,16 @@ import com.empire.authentication.configurations.security.jwt.JwtConfigurer;
 import com.empire.authentication.configurations.security.jwt.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
