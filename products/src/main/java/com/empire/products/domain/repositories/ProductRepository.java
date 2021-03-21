d06a47ed5b01054fbc3f14e60f06f12db94a2eb1
Ruan-fe <ruanfelipetottiamaral@gmail.com>
2021-03-21 00:06:37 -0300

    Removendo products por conta que está dentro do sale-service

--- products/src/main/java/com/empire/products/domain/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.empire.products.domain.repositories;
-
-import com.empire.products.domain.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
