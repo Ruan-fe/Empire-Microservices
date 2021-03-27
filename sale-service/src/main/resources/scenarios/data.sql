c02efe52237573c3e67ecc3e86f3576c6863465c
Ruan-fe <ruanfelipetottiamaral@gmail.com>
2021-03-26 23:48:05 -0300

    Classes de teste/criando testes unitários

---
+++ sale-service/src/main/resources/scenarios/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO CATEGORY(id,description) VALUES(1,'Placa de vídeo');
+INSERT INTO PRODUCT(id,description,value,quantity_stock,category_id) VALUES(1,'GTX 750 TI',500,5,1);
+INSERT INTO PRODUCT(id,description,value,quantity_stock,category_id) VALUES(2,'GTX 1050 TI',800,10,1);
---
+++ sale-service/src/test/java/com/saleservice/product/ProductControllerTest.java
@@ -0,0 +1,67 @@
+package com.saleservice.product;
+
+import com.saleservice.helper.MockMvcHelper;
+import com.saleservice.rest.models.requests.ProductRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Sql(value = "classpath:scenarios/data.sql")
+@EnableAutoConfiguration(exclude = SecurityAutoConfiguration.class)
+public class ProductControllerTest {
+
+    private final String path = "/sale/product";
+
+    @Autowired
+    private MockMvcHelper mockMvcHelper;
+
+
+    @Test
+    public void shouldReturnOkWhenSearchToProducts() throws Exception {
+
+        mockMvcHelper.get(path)
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].description").value("GTX 1050 TI"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].value").value(800))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].quantityStock").value(10))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].category.id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[0].category.description").value("Placa de vídeo"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].description").value("GTX 750 TI"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].value").value(500))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].quantityStock").value(5))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].category.id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content[1].category.description").value("Placa de vídeo"));
+    }
+
+    @Test
+    public void shouldReturnCreatedWhenInsertAProduct() throws Exception {
+
+        ProductRequest productRequest = ProductRequest.builder().description("GTX 650 TI").value(450.0).quantityStock(20L).categoryId(1L).build();
+
+        mockMvcHelper.save(path,productRequest)
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(3))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.description").value("GTX 650 TI"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.value").value(450))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.quantityStock").value(20))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.category.id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.category.description").value("Placa de vídeo"));
+    }
+
+
+
+
+}
