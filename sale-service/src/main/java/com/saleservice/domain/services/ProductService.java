e0358e1ae34997e6a64517e2653c0de008f9aba1
Ruan-fe <ruanfelipetottiamaral@gmail.com>
2021-03-27 18:27:35 -0300

    Adicionando excessão personalizada

--- sale-service/src/main/java/com/saleservice/domain/services/ProductService.java
+++ sale-service/src/main/java/com/saleservice/domain/services/ProductService.java
@@ -1,5 +1,6 @@
 package com.saleservice.domain.services;
 
+import com.saleservice.configurations.exceptions.ResourceNotFoundException;
 import com.saleservice.domain.entities.Category;
 import com.saleservice.domain.entities.Product;
 import com.saleservice.domain.repositories.CategoryRepository;
@@ -28,7 +29,7 @@ public class ProductService {
 
     public Product save(ProductRequest productRequest) {
 
-        Category category = categoryRepository.findById(productRequest.getCategoryId()).orElseThrow(()-> new RuntimeException());
+        Category category = categoryRepository.findById(productRequest.getCategoryId()).orElseThrow(()-> new ResourceNotFoundException("Category not found!"));
 
         Product product = productRequest.convert(category);
 
@@ -37,7 +38,7 @@ public class ProductService {
 
     public Product change(ProductChangeRequest productChangeRequest, Long productId) {
 
-        Category category = categoryRepository.findById(productChangeRequest.getCategoryId()).orElseThrow(()-> new RuntimeException());
+        Category category = categoryRepository.findById(productChangeRequest.getCategoryId()).orElseThrow(()-> new ResourceNotFoundException("Category not found!"));
         Product product = productChangeRequest.convert(category,productId);
         return  productRepository.save(product);
 
